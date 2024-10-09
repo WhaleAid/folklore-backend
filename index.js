6a19b38e141d41f809328816f25f0f04e1e44181
WalidKhalqallah <wkhalqallah2@myges.fr>
2024-10-09 13:12:36 +0200

    fix server function

--- index.js
+++ index.js
@@ -61,7 +61,9 @@ app.post('/webhook', express.raw({ type: 'application/json' }), async (req, res)
 });
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+}));
 app.set('trust proxy', true);
 
 app.get('/', (req, res) => {
